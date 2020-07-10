e51955c1d542a262b0db6b4c456b14d355c4f935
Helena Oliveira <helena.oliveira@casaldev.com.br>
2020-07-10 14:08:41 -0300

    chore: change repo to monorepo using yarn workspaces

---
+++ packages/server/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '80c1c82fc4cbffad8e0ffddb6620f902',
+    expiresIn: '1d',
+  },
+};
---
+++ packages/web/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <SignUp />
+      <GlobalStyle />
+    </>
+  );
+};
+
+export default App;
