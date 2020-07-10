07f60c698d1a265ba126f945aed5f78cf9470901
Helena Oliveira <helena.oliveira@casaldev.com.br>
2020-07-10 20:22:04 -0300

    fix: typeorm configuration

--- packages/server/src/database/index.ts
+++ packages/server/src/database/index.ts
@@ -1,3 +1,18 @@
 import { createConnection } from 'typeorm';
 
-createConnection();
+createConnection({
+  type: "postgres",
+  host: "localhost",
+  port: 5432,
+  username: "postgres",
+  password: "docker",
+  database: "gostack_gobarber",
+  entities: [
+    "./src/models/*.ts"
+ ],
+  migrations: [
+    "./src/database/migrations/*.ts"
+ ],
+ cli: {
+   "migrationsDir": "./src/database/migrations"
+}});
