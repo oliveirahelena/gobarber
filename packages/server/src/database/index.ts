7b902c7aa4007a0b912227360a437c0cf6ce0795
Helena Oliveira <helena.oliveira@casaldev.com.br>
2020-07-12 06:48:37 -0300

    feature: web: sign in form validation

--- packages/server/src/database/index.ts
+++ packages/server/src/database/index.ts
@@ -1,18 +1,15 @@
 import { createConnection } from 'typeorm';
 
 createConnection({
-  type: "postgres",
-  host: "localhost",
+  type: 'postgres',
+  host: 'localhost',
   port: 5432,
-  username: "postgres",
-  password: "docker",
-  database: "gostack_gobarber",
-  entities: [
-    "./src/models/*.ts"
- ],
-  migrations: [
-    "./src/database/migrations/*.ts"
- ],
- cli: {
-   "migrationsDir": "./src/database/migrations"
-}});
+  username: 'postgres',
+  password: 'docker',
+  database: 'gostack_gobarber',
+  entities: ['./src/models/*.ts'],
+  migrations: ['./src/database/migrations/*.ts'],
+  cli: {
+    migrationsDir: './src/database/migrations',
+  },
+});
