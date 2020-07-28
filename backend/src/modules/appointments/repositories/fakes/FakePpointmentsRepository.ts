5c24f83bf505a597a0e800bb38bab34a384c465a
Helena Oliveira <helena.oliveira@casaldev.com.br>
2020-07-28 06:11:18 -0300

    implement test coverage

--- backend/src/modules/appointments/repositories/fakes/FakePpointmentsRepository.ts
+++ backend/src/modules/appointments/repositories/fakes/FakePpointmentsRepository.ts
@@ -1,16 +1,17 @@
 import { uuid } from 'uuidv4';
+import { isEqual } from 'date-fns';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
 
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 
-class AppointmentRepository implements IAppointmentsRepository {
+class FakeAppointmentRepository implements IAppointmentsRepository {
   private appointments: Appointment[] = [];
 
   public async findByDate(date: Date): Promise<Appointment | undefined> {
-    const findAppointment = this.appointments.find(
-      appointment => appointment.date === date,
+    const findAppointment = this.appointments.find(appointment =>
+      isEqual(appointment.date, date),
     );
 
     return findAppointment;
@@ -30,4 +31,4 @@ class AppointmentRepository implements IAppointmentsRepository {
   }
 }
 
-export default AppointmentRepository;
+export default FakeAppointmentRepository;
